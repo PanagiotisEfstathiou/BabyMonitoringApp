bac8c4ff375de39f1a067826c34c7b2bc1eae9cd
PanagiotisEfstathiou <pefstathiou@outlook.com>
2023-05-02 23:31:19 +0300

    Initial commit

---
+++ src/main/java/com/edu/babymonitoringapp/BabyMonitoringAppApplication.java
@@ -0,0 +1,13 @@
+package com.edu.babymonitoringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabyMonitoringAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BabyMonitoringAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/edu/babymonitoringapp/BabyMonitoringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.babymonitoringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabyMonitoringAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
