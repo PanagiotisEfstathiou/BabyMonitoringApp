315bc45becf5fb0d402743eb24e09a9502c5ac6e
Panagiotis Efstathiou <pefstathiou@outlook.com>
2023-05-03 19:08:09 +0300

    creates a findByUsername method

--- src/main/java/com/edu/babymonitoringapp/repositoy/AccountRepository.java
+++ src/main/java/com/edu/babymonitoringapp/repositoy/AccountRepository.java
@@ -2,9 +2,14 @@ package com.edu.babymonitoringapp.repositoy;
 
 import com.edu.babymonitoringapp.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
+    @Query("SELECT a FROM Account a WHERE a.username = ?1")
+    Optional<Account> findByUsername(String username);
 }
