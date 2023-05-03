ad5bbe3d43fc82e8b039f166ea6f1c61ce3a51b9
Panagiotis Efstathiou <pefstathiou@outlook.com>
2023-05-03 19:07:42 +0300

    Makes username of account unique

--- src/main/java/com/edu/babymonitoringapp/model/Account.java
+++ src/main/java/com/edu/babymonitoringapp/model/Account.java
@@ -5,6 +5,7 @@ import jakarta.xml.bind.annotation.XmlAccessType;
 import jakarta.xml.bind.annotation.XmlAccessorType;
 import jakarta.xml.bind.annotation.XmlRootElement;
 import lombok.Data;
+import org.hibernate.validator.constraints.UniqueElements;
 
 @Entity
 @XmlAccessorType(XmlAccessType.NONE)
@@ -17,7 +18,9 @@ public class Account {
     @Column(name = "id", nullable = false)
     private Long id;
 
+    @Column(unique=true)
     private String username;
+
     private String password;
     private Role role;
 }
