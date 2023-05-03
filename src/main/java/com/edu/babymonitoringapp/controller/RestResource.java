9c029576b70520a654e59715452c755848fa045f
Panagiotis Efstathiou <pefstathiou@outlook.com>
2023-05-03 19:10:43 +0300

    creates some methods in the controller

--- src/main/java/com/edu/babymonitoringapp/controller/RestResource.java
+++ src/main/java/com/edu/babymonitoringapp/controller/RestResource.java
@@ -6,20 +6,22 @@ import com.edu.babymonitoringapp.service.RecordService;
 import jakarta.annotation.security.DenyAll;
 import jakarta.annotation.security.PermitAll;
 import jakarta.annotation.security.RolesAllowed;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Response;
 import jakarta.xml.bind.annotation.XmlAccessType;
 import jakarta.xml.bind.annotation.XmlAccessorType;
 import jakarta.xml.bind.annotation.XmlRootElement;
 import java.net.URI;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 //
-@XmlAccessorType(XmlAccessType.NONE)
-@XmlRootElement(name = "RestResource")
+//@XmlAccessorType(XmlAccessType.NONE)
+//@XmlRootElement(name = "RestResource")
 @Path("/rest")
+@CrossOrigin(origins = "*", allowedHeaders = "*", allowCredentials = "true")
 public class RestResource {
 
     @Autowired
@@ -31,18 +33,29 @@ public class RestResource {
     @POST
     @Consumes("application/json")
     @Path("/createAccount")
+//    @CrossOrigin(origins = "*", allowedHeaders = "*", allowCredentials = "true")
     public Response createAccount(AccountDto accountDto) {
         accountService.createAccount(accountDto);
 
         return Response.status(201)
+                .status(Response.Status.CREATED)
+//                .header("Access-Control-Allow-Origin", "*")
+//                .header("Access-Control-Allow-Credentials", "true")
+//                .header("Access-Control-Allow-Headers",
+//                        "origin, content-type, accept, authorization")
+//                .header("Access-Control-Allow-Methods",
+//                        "GET, POST, PUT, DELETE, OPTIONS, HEAD")
                 .entity("Account created successfully !!").build();
     }
 
     @PermitAll
     @GET
     @Path("/getAllAccounts")
-    public Response getAllAccounts() {
-        return Response.ok(accountService.getAllAccounts()).build();
+    @CrossOrigin(origins = "*", allowedHeaders = "*", allowCredentials = "true")
+    @Produces("application/json")
+    public List<AccountDto> getAllAccounts() {
+        return accountService.getAllAccounts();
+
     }
 
     @PermitAll
@@ -51,8 +64,32 @@ public class RestResource {
     @Path("/login")
     public Response login(AccountDto accountDto) {
         //accountService.login(accountDto);
+        if (accountService.login(accountDto)) {
+            return Response.status(201)
+                    .entity("Login successfully !!").build();
+        } else {
+            return Response.status(401)
+                    .entity("Login failed !!").build();
+        }
 
-        return Response.status(201)
-                .entity("Account created successfully !!").build();
+    }
+
+    @PermitAll
+    @GET
+    @Path("/getAccountById/{id}")
+    public Response getAccountById(@PathParam("id") Long id) {
+        return Response.ok(accountService.getAccountById(id)).build();
+    }
+
+    @OPTIONS
+    @Path("{path : .*}")
+    public Response options() {
+        return Response.ok("")
+                .header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+                .header("Access-Control-Allow-Credentials", "true")
+                .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+                .header("Access-Control-Max-Age", "1209600")
+                .build();
     }
 }
